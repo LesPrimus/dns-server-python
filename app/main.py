e7761e71059a741ba2e4956535c5038f58cb908e
LesPrimus <amerigo.armentano@hotmail.it>
2025-08-13 06:29:21 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,19 @@
 import socket
+import struct
+from dataclasses import dataclass, astuple
 
+@dataclass(frozen=True)
+class DNSHeader:
+    id: int
+    flags: int
+    question_count: int
+    answer_count: int
+    authority_count: int
+    additional_count: int
+
+    @property
+    def as_bytes(self):
+        return struct.pack("!HHHHHH", *astuple(self))
 
 def main():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -9,7 +23,14 @@ def main():
         try:
             buf, source = udp_socket.recvfrom(512)
 
-            response = b""
+            response = DNSHeader(
+                id=1234,
+                flags=0x8180,
+                question_count=0,
+                answer_count=0,
+                authority_count=0,
+                additional_count=0,
+            ).as_bytes
 
             udp_socket.sendto(response, source)
         except Exception as e:
